44dfdf4499aec4f570d895e099199a952c22caf4
mohammed.o <mohammed.o@DESKTOP-LLGMAUI>
2020-03-11 14:06:18 +0000

    jawad1

--- Test Suites/jawad1.ts
+++ Test Suites/jawad1.ts
@@ -10,4 +10,10 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>f9f77542-2efa-42b3-8188-25dfdf737ec3</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca02998c-61a5-4ba5-8240-ffe275c64cc7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/case jawad</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
