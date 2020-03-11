1255da48140e6161e71b8b4750980f876059af44
souad.r <souad.r@DESKTOP-43S45TG>
2020-03-11 13:52:29 +0000

    souad test

---
+++ Test Suites/suite souad.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suite souad</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a980a2cc-52ae-45fd-88e8-a9fa4971b66c</testSuiteGuid>
+</TestSuiteEntity>
